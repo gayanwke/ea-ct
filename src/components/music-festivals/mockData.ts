f54a850d04a1ab38ea82c81ada6f0195cf32da83
Richard Snazell <richard.snazell@gmail.com>
2021-02-11 07:53:25 +1000

    add mockData

---
+++ src/components/music-festivals/mockData.ts
@@ -0,0 +1,37 @@
+import { MusicFestival } from './MusicFestivals.types';
+
+export const mockData: MusicFestival[] = [
+  {
+    name: 'Trainerella',
+    bands: [
+      { name: 'Adrian Venti', recordLabel: 'Monocracy Records' },
+      { name: 'YOUKRANE', recordLabel: 'Anti Records' },
+      { name: 'Yanke East', recordLabel: 'MEDIOCRE Music' },
+    ],
+  },
+  {
+    name: 'LOL-palooza',
+    bands: [
+      { name: 'Winter Primates', recordLabel: '' },
+      { name: 'Werewolf Weekday', recordLabel: 'Pacific Records' },
+      { name: 'Jill Black', recordLabel: 'Fourth Woman Records' },
+      { name: 'Yanke East', recordLabel: 'MEDIOCRE Music' },
+      { name: 'Frank Jupiter', recordLabel: 'Pacific Records' },
+    ],
+  },
+  {
+    name: 'Twisted Tour',
+    bands: [
+      { name: 'Auditones', recordLabel: 'MEDIOCRE Music' },
+      { name: 'Summon', recordLabel: 'Pacific Records' },
+      { name: 'Yanke East', recordLabel: 'MEDIOCRE Music' },
+      { name: 'Squint-281' },
+    ],
+  },
+  {
+    bands: [
+      { name: 'Critter Girls', recordLabel: 'Anti Records' },
+      { name: 'Propeller', recordLabel: 'Pacific Records' },
+    ],
+  },
+];
